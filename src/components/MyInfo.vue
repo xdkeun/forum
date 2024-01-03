7be4f5a49afe1cf36e8a37262c508fa031553652
xdkeun <dragonkeun7@gmail.com>
2024-01-03 10:49:34 +0900

    feat: 내정보, 이미지 컴포넌트 구현

--- src/components/MyInfo.vue
+++ src/components/MyInfo.vue
@@ -1,14 +1,20 @@
 <template>
-  <Input :type="'text'" :icon="'fa-solid fa-user'" :placeholder="'아이디'" @input-change="idChangeHandler" />
-  <Input :type="'password'" :icon="'fa-solid fa-lock'" :placeholder="'비밀번호'" @input-change="passwordChangeHandler" />
-  <Input :type="'text'" :icon="'fa-solid fa-user'" :placeholder="'닉네임'" @input-change="nicknameChangeHandler" />
-  <Button text="수정" @click="buttonClickHandler" />
+  <div class="myinfo-layout">
+    <BigText text="내정보" />
+    <Input :type="'text'" :icon="'fa-solid fa-user'" :placeholder="'아이디'" @input-change="idChangeHandler" />
+    <Input :type="'password'" :icon="'fa-solid fa-lock'" :placeholder="'비밀번호'" @input-change="passwordChangeHandler" />
+    <Input :type="'text'" :icon="'fa-solid fa-user'" :placeholder="'닉네임'" @input-change="nicknameChangeHandler" />
+    <Image text="프로필사진" />
+    <Button text="정보수정" @click="buttonClickHandler" />
+  </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+import BigText from './BigText.vue';
 import Input from './Input.vue';
 import Button from './Button.vue';
-import { ref } from 'vue';
+import Image from './Image.vue';
 const id = ref("")
 const password = ref("")
 const nickname = ref("")
@@ -19,3 +25,13 @@ const buttonClickHandler = () => {
   console.log(id.value, password.value, nickname.value)
 }
 </script>
+
+<style scoped>
+.myinfo-layout {
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+}
+</style>
