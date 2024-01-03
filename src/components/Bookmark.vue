bca8f05e328641ec5bc690c27dbb5d8850ede33a
xdkeun <dragonkeun7@gmail.com>
2024-01-03 10:56:12 +0900

    feat: Bookmark 컴포넌트 구현

--- src/components/Bookmark.vue
+++ src/components/Bookmark.vue
@@ -1,3 +1,60 @@
 <template>
-  Bookmark
+  <div class="bookmark-layout">
+    <ul class="post-wrapper">
+      <li class="text title">제목</li>
+      <li class="text bookmark-header">북마크</li>
+    </ul>
+    <ul class="post-wrapper" v-for="(post, index) in postList" :key="index">
+      <li class="text title">{{ post.title }}</li>
+      <i class="fa-solid fa-star text bookmark yellow"></i>
+    </ul>
+  </div>
 </template>
+
+<script setup>
+import { ref } from "vue";
+import postlist from "@/constants/postlist"
+const postList = ref(postlist);
+</script>
+<style scoped>
+.bookmark-layout {
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+}
+
+.post-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 30px;
+  gap: 5px;
+  border-bottom: 0.5px solid darkgray;
+}
+
+
+.title {
+  width: 300px;
+}
+
+.bookmark-header {
+  width: 50px;
+}
+
+.bookmark {
+  width: 50px;
+  color: darkgray;
+}
+
+.yellow {
+  color: #ffd400;
+}
+
+.text {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+</style>
